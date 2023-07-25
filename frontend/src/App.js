2e0c36075f3a6611efcf4502591a5bbff1a78602
Steve Leonard <github@leonarduk.com>
2023-07-25 12:07:07 +0100

    JD-93: Implement secure user authentication and access control

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-// App.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import Login from './Login'; // Import the Login component
 
-function App() {
+const App = () => {
+  const [isLogin, setIsLogin] = useState(true); // Set initial mode to Login
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
 
   const handleSignUp = async () => {
-    // Your existing handleSignUp function for sign up
     try {
-      const response = await axios.post(
-        'http://localhost:4000/dev/signup',
-        {
-          name: name,
-          email: email,
-          password: password,
-        }
-      );
+      const response = await axios.post('http://localhost:4000/dev/signup', {
+        name,
+        email,
+        password,
+      });
       console.log(response.data.message);
       // Display welcome message here or redirect to another page after successful sign-up.
     } catch (error) {
@@ -27,27 +24,73 @@ function App() {
     }
   };
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/dev/login', {
+        username,
+        password: loginPassword,
+      });
+      // Handle successful login, e.g., store session token in cookies and redirect to dashboard.
+      console.log(response.data); // The response should contain the session token or user info.
+    } catch (error) {
+      // Handle login failure, e.g., show error message.
+      console.error(error.message);
+    }
+  };
+
+  const toggleMode = () => {
+    setIsLogin((prev) => !prev); // Toggle between Login and Sign Up mode
+  };
+
   return (
     <div>
-      <h1>JobSearch Dashboard Sign Up</h1>
-      <div>
-        <label>Name:</label>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <button onClick={handleSignUp}>Sign Up</button>
+      <h1>JobSearch Dashboard</h1>
 
-      {/* Render the Login component */}
-      <Login />
+      {/* Sign Up or Login Form based on mode */}
+      {isLogin ? (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div>
+              <label>Username:</label>
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div>
+              <label>Password:</label>
+              <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+          <p>
+            Don't have an account?{' '}
+            <button onClick={toggleMode}>Register</button>
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h2>Sign Up</h2>
+          <div>
+            <label>Name:</label>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button onClick={handleSignUp}>Sign Up</button>
+          <p>
+            Already have an account?{' '}
+            <button onClick={toggleMode}>Login</button>
+          </p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
