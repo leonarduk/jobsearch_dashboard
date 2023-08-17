d42793fc3b539c7c88efb4d71a2e69ddd367eb1a
Steve Leonard <github@leonarduk.com>
2023-08-17 08:28:52 +0100

    JD108: improve error handling

--- jd-backend/aws_utils.py
+++ jd-backend/aws_utils.py
@@ -1,24 +1,36 @@
+import logging
 import base64
 import json
-
 import chardet
 import os
 import boto3
 from botocore.exceptions import ClientError
 
+# Initialize the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+# Define a custom exception class for better error handling
+class CustomException(Exception):
+    pass
+
+
+def log_and_raise_error(message):
+    logger.error(message)
+    raise CustomException(message)
+
 
 def get_dynamo_client():
-    # Check if the function is running locally (offline)
-    is_offline = os.environ.get('IS_OFFLINE')
-    print(is_offline)
-    # Set up the DynamoDB resource based on the environment
-    if is_offline:
-        dynamodb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
-    else:
-        dynamodb = boto3.client('dynamodb')
-
-    print(str(dynamodb))
-    return dynamodb
+    try:
+        is_offline = os.environ.get('IS_OFFLINE')
+        if is_offline:
+            dynamodb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
+        else:
+            dynamodb = boto3.client('dynamodb')
+        return dynamodb
+    except Exception as e:
+        log_and_raise_error(f"Failed to get DynamoDB client: {e}")
 
 
 def add_item_to_table(dynamodb_client, table, item):
@@ -58,39 +70,43 @@ def get_dynamodb_resource():
 
 
 def get_data_from_body(event):
-    if "body" in event:
-        body = event['body']
-        print("body: " + str(body))
+    try:
+        if "body" in event:
+            body = event['body']
+            logger.info("body: " + str(body))
 
-        if "isBase64Encoded" in event and event['isBase64Encoded']:
+            if "isBase64Encoded" in event and event['isBase64Encoded']:
                 decoded_data = base64.b64decode(body)
-                # Use chardet to detect the character encoding
                 result = chardet.detect(decoded_data)
                 encoding = result['encoding']
                 request_body = decoded_data.decode(encoding)
-        else:
-            request_body = body
+            else:
+                request_body = body
+
             if isinstance(request_body, dict):
                 return request_body
 
-        data = json.loads(request_body)
-
-    else:
-        data = event
-
-    return data
+            data = json.loads(request_body)
+            return data
+        else:
+            return event
+    except Exception as e:
+        log_and_raise_error(f"Failed to get data from body: {e}")
 
 
 def create_response(message, status_code):
-    print(f"{status_code}: {message}")
-    response = {
-        "statusCode": status_code,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",  # Allow requests from any domain
-            "Access-Control-Allow-Headers": "Content-Type",  # Add any required headers here
-            "Access-Control-Allow-Methods": "OPTIONS,POST",  # Add allowed HTTP methods here
-            "Access-Control-Allow-Credentials": True,  # Allow sending of cookies
-        },
-        "body": json.dumps({"message": message})
-    }
-    return response
+    try:
+        logger.info(f"{status_code}: {message}")
+        response = {
+            "statusCode": status_code,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,POST",
+                "Access-Control-Allow-Credentials": True,
+            },
+            "body": json.dumps({"message": message})
+        }
+        return response
+    except Exception as e:
+        log_and_raise_error(f"Failed to create response: {e}")
